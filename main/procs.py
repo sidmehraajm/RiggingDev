afcc9cf48d3cdf412f9214024ac622fbd9cbf5f7
Siddarth Mehra <51001520+sidmehraajm@users.noreply.github.com>
2023-08-02 23:58:59 -0400

    temp proc objs
    proc start,
    window start

--- main/procs.py
+++ main/procs.py
@@ -1,3 +1,6 @@
+import pymel.core as pm
+import maya.cmds as cmds
+
 '''
 
 ui
@@ -8,6 +11,31 @@ one defination for all deformer
 skin cluster utils 
 export/import- skin/deformers 
 copy skin 
-
+vertex weight slider
 '''
+
+class find_data:
+    '''
+    TODO write doc
+    '''
+    def __init__(self,mesh):
+        self.mesh = mesh
+
+class vn_skin_utils:
+    '''
+    TODO write doc
+    '''
+    def __init__(self):
+        self.holdjoint = '_hold_jnt'
+        self.prefix = '_skn_tmp_jnt'
+
+    def create_joint(self,pos):
+        jnt = pm.createNode('joint')
+        
+
+
+
+
+    
+
 
---
+++ main/ui.py
@@ -0,0 +1,29 @@
+import pymel.core as pm
+win_name = 'vn_skin_tools'
+win_title = 'Skin Tools'
+win_size = (350,500)
+if(pm.window(win_name, exists=True)):
+    pm.deleteUI(win_name, window=True)
+pm.window(win_name, title=win_title, widthHeight=win_size, sizeable=False)
+
+main_form = pm.formLayout(numberOfDivisions=100)
+deformer_frame = pm.frameLayout(parent=main_form, label='Deformer To Skin', collapsable=True)
+skin_utils_frame = pm.frameLayout(parent=main_form, label='Weights Import/Export', collapsable=True)  
+output_win_frame = pm.frameLayout(parent=main_form, label='Weights Mirror', collapsable=True) 
+          
+pm.formLayout(main_form, edit=True,
+                attachForm=([deformer_frame, 'top', 0], [deformer_frame, 'left', 0], [deformer_frame, 'right', 0], [skin_utils_frame, 'left', 0], 
+                [skin_utils_frame, 'right', 0], [output_win_frame, 'left', 0], [output_win_frame, 'right', 0]),
+                attachControl=([skin_utils_frame, 'top', 10, deformer_frame], [output_win_frame, 'top', 10, skin_utils_frame]))
+radio_layout = pm.rowColumnLayout(nc =3,p = deformer_frame)
+radio_coll = pm.radioCollection( parent=radio_layout )
+crv_to_skn_btn = cmds.radioButton(l = 'Curve To Skin', p = radio_layout,sl =1)
+wire_to_skn_btn= cmds.radioButton(l = 'Wire To Skin', p = radio_layout)
+softsel_to_skn = cmds.radioButton(l = 'Soft Selection To Skin', p = radio_layout)
+lat_to_skn_btn = cmds.radioButton(l = 'Lattice To Skin', p = radio_layout)
+wrap_to_skn = cmds.radioButton(l = 'Wrap To Skin', p = radio_layout)
+blendshape_to_skn = cmds.radioButton(l = 'Blendshape To Skin', p = radio_layout)
+cluster_to_skn = cmds.radioButton(l = 'Cluster To Skin', p = radio_layout)
+
+    
+pm.showWindow(win_name)
